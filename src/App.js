5f4efbc6340a0e2cd69d310122b60ce720989907
jtx007 <jamesjacobthomas7@gmail.com>
2018-09-27 12:29:03 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import Quote from './containers/Quotes'
+import QuoteForm from './containers/QuoteForm'
 class App extends Component {
   render() {
     return (
@@ -8,14 +9,13 @@ class App extends Component {
           <h1>Quote Maker</h1>
         </div>
         <hr />
-        {/*
-          TODO:
-
-          Render QuoteForm and Quotes Components
-         */}
+        <QuoteForm />
+        <Quote />
       </div>
     );
   }
 }
+
+
 
 export default App;
